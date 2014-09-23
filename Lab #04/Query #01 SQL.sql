cc982edd1a7f763bfddc6a09bc50774f66a424bf
Peter J. Ramaglia <peter.j.ramaglia@outlook.com>
2014-09-23 16:54:41 -0400

    Lab #04
    Submitting the first query of Lab #04.  Receiving the following error
    message for some reason:
    
    > ERROR:  more than one row returned by a subquery used as an expression
    > ********** Error **********
    >
    > ERROR: more than one row returned by a subquery used as an expression
    > SQL state: 21000

---
+++ Lab #04/Query #01 SQL.sql
@@ -0,0 +1,25 @@
+﻿-- 1. Get the cities of agents booking an order for customer "TipTop."
+select city
+from agents
+where aid = (select aid
+             from orders
+             where cid = (select cid
+                          from customers
+                          where name = 'Tiptop'
+                          )
+             )
+
+select *
+from orders
+where cid = (select cid
+             from customers
+             where name = 'Tiptop'
+             )
+
+select distinct aid
+from orders
+where cid = (select cid
+             from customers
+             where name = 'Tiptop'
+             )
+
